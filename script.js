e012f10c1384aad83dd9adfac0d5d5c92a3d13ff
momokokong <momokokong@gmail.com>
2024-06-26 11:07:24 -0700

    Add comments in script.js

--- script.js
+++ script.js
@@ -2,6 +2,17 @@
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
 
 // Collect employee data
+// function: collectEmployees
+// parameter: None
+// return: an array:listOfEmployee of object:employee containing employee info(firstName, lastName, salary)
+// Descriptions: 
+// boolean: addMore (init as true) determines whether or not the user wants to add more employee.
+// Then the flow enters a while loop with condition being addMore.  Within the while loop it prompt the user to 
+// enter firstName, lastName, and salary.  For each entry it check whether it's an valid input.
+// firstName and lastName can be anything as long as the user does not cancel (NULL).  Salary must be a number
+// greater or eaqual to 0 and not NULL. If the user chooses cancel, the javascrip would quit from that point.
+// Once the user entered three valid answers, it push the object:employee to listOfEmployee then check with 
+// the user whether to add more. 
 const collectEmployees = function() {
   let addMore = true;
   let listOfEmployee = [];
@@ -42,6 +53,13 @@ const collectEmployees = function() {
 }
 
 // Display the average salary
+// function: displayAverageSalary
+// parameter: employeesArray: an array of object:employee containing employee info(firstName, lastName, salary)
+// return: None
+// Descriptions: 
+// The function takes in the employeesArray and add each employee's salary to a total, then divide it by the 
+// numbers of employee(employeesArray) to get the average. The function does not return anything but would 
+// output the average to console. 
 const displayAverageSalary = function(employeesArray) {
   let average;
   let total = 0;
@@ -53,6 +71,13 @@ const displayAverageSalary = function(employeesArray) {
 }
 
 // Select a random employee
+// function: getRandomEmployee
+// parameter: employeesArray: an array of object:employee containing employee info(firstName, lastName, salary)
+// return: None
+// Descriptions: 
+// The random number is determined by Math.random() * employeesArray.length then floor it to an integer. Therefore, 
+// it's always any integer from 0 to (employeesArray.length - 1) and used as the index for the lucky winner!  
+// The function does not return anything but would output the info of the lucky winner.
 const getRandomEmployee = function(employeesArray) {
   const randomEmployee = Math.floor(Math.random() * employeesArray.length);
   console.log("Congratulations to " + employeesArray[randomEmployee].firstName + " " + employeesArray[randomEmployee].lastName + ", our random drawing winner!");
